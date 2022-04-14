c276667aa5bd99c97fc7c983daee48e3174843a0
ThatoMahloko <lordwaterphfffteat@gmail.com>
2022-04-14 10:26:26 +0200

    pushing

--- src/components/FormAddCells.jsx
+++ src/components/FormAddCells.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../App.css'
+import { RiVirusLine } from "react-icons/ri";
 
 const FormAddCells = ({ addEntry }) => {
     const [firstName, setFirstName] = useState('');
@@ -27,44 +28,43 @@ const FormAddCells = ({ addEntry }) => {
         setBreathing('');
         setID('');
     }
-
     return (
         <div>
-
+            <RiVirusLine  className="icon"/>
             <form className="container" onSubmit={handleSubmit}>
                 <div className="left ">
-                    <input type="text" placeholder="First Name" required onChange={(e) => setFirstName(e.target.value)} />
-                    <input type="text" placeholder="Last Name" required onChange={(e) => setLastName(e.target.value)} />
-                    <input type="text" placeholder="ID number" required onChange={(e) => setIdNumber(e.target.value)} />
+                    <input type="text" placeholder="First Name" required onChange={(firstName) => setFirstName(firstName)} />
+                    <input type="text" placeholder="Last Name" required onChange={(lastName) => setLastName(lastName)} />
+                    <input type="text" placeholder="ID number" required onChange={(id) => setIdNumber(id)} />
                 </div>
 
                 <div className="right ">
-                    <input type="email" placeholder="Email Address" required onChange={(e) => setEmailAddress(e.target.value)} />
-                    <input type="date" placeholder="Date" required onChange={(e) => setDate(e.target.value)} />
-                    <input type="time" placeholder="time" required onChange={(e) => setTime(e.target.value)} />
+                    <input type="email" placeholder="Email Address" required onChange={(emailAddress) => setEmailAddress(emailAddress)} />
+                    <input type="date" placeholder="Date" required onChange={(date) => setDate(date)} />
+                    <input type="time" placeholder="time" required onChange={(time) => setTime(time)} />
                 </div>
 
                 <div>
                     <label>
                         Do you have a sore throat?<br />
-                        <input id="soreYes" type="radio" value="yes" checked={soreThroat === "yes"} onChange={(e) => setSoreThroat(e.target.value)} /><label htmlFor="soreYes">yes</label>
-                        <input id="soreNo" type="radio" value="no" checked={soreThroat === "no"} onChange={(e) => setSoreThroat(e.target.value)} /><label htmlFor="soreNo">no</label>
+                        <input id="soreYes" type="radio" value="yes" checked={soreThroat === "yes"} onChange={(soreThroat) => setSoreThroat(soreThroat)} /><label htmlFor="soreYes">yes</label>
+                        <input id="soreNo" type="radio" value="no" checked={soreThroat === "no"} onChange={(soreThroat) => setSoreThroat(soreThroat)} /><label htmlFor="soreNo">no</label>
                     </label>
                 </div>
 
                 <div>
                     <label>
                         Do you have a cough throat?<br />
-                        <input id="coughYes" type="radio" value="yes" checked={cough === "yes"} onChange={(e) => setCough(e.target.value)} /><label htmlFor="coughYes">yes</label>
-                        <input id="coughNo" type="radio" value="no" checked={cough === "no"} onChange={(e) => setCough(e.target.value)} /><label htmlFor="coughNo">no</label>
+                        <input id="coughYes" type="radio" value="yes" checked={cough === "yes"} onChange={(cough) => setCough(cough)} /><label htmlFor="coughYes">yes</label>
+                        <input id="coughNo" type="radio" value="no" checked={cough === "no"} onChange={(cough) => setCough(cough)} /><label htmlFor="coughNo">no</label>
                     </label>
                 </div>
 
                 <div>
                     <label>
                         Do you have toruble breathing?<br />
-                        <input id="breathingYes" type="radio" value="yes" checked={breathing === "yes"} onChange={(e) => setBreathing(e.target.value)} /><label htmlFor="soreYes">yes</label>
-                        <input id="breatingNo" type="radio" value="no" checked={breathing === "no"} onChange={(e) => setBreathing(e.target.value)} /><label htmlFor="soreNo">no</label>
+                        <input id="breathingYes" type="radio" value="yes" checked={breathing === "yes"} onChange={(breathing) => setBreathing(breathing)} /><label htmlFor="soreYes">yes</label>
+                        <input id="breatingNo" type="radio" value="no" checked={breathing === "no"} onChange={(breathing) => setBreathing(breathing)} /><label htmlFor="soreNo">no</label>
                     </label>
                 </div>
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,40 +15,42 @@ const Table = () => {
     }
 
     return (
-        <div className="">
+        <div>
             <FormAddCells addEntry={addEntry} />
-            {cells.length === 0 ? <div> </div> :
-                <table>
-                    <thead>
-                        <tr>
-                            <th>first name</th>
-                            <th>last name</th>
-                            <th>id number</th>
-                            <th>email address</th>
-                            <th>date</th>
-                            <th>time</th>
-                            <th>sore throat</th>
-                            <th>cough</th>
-                            <th>trouble breating</th>
-                        </tr>
-                        {cells.map(entry => {
-                            <tr key={entry.id}>
-                                <td>{entry.firstName}</td>
-                                <td>{entry.lastName}</td>
-                                <td>{entry.idNumber}</td>
-                                <td>{entry.emailAddress}</td>
-                                <td>{entry.date}</td>
-                                <td>{entry.time}</td>
-                                <td>{entry.soreThroat}</td>
-                                <td>{entry.cough}</td>
-                                <td>{entry.breathing}</td>
-                                <td><button onClick={handleDelete}>DELETE</button></td>
+            <div className="table">
+                {cells.length === 0 ? <div> </div> :
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>first name</th>
+                                <th>last name</th>
+                                <th>id number</th>
+                                <th>email address</th>
+                                <th>date</th>
+                                <th>time</th>
+                                <th>sore throat</th>
+                                <th>cough</th>
+                                <th>trouble breating</th>
                             </tr>
-                        })}
-                    </thead>
-                </table>
-            }
+                            {cells.map(entry => {
+                                <tr key={entry.id}>
+                                    <td>{entry.firstName}</td>
+                                    <td>{entry.lastName}</td>
+                                    <td>{entry.idNumber}</td>
+                                    <td>{entry.emailAddress}</td>
+                                    <td>{entry.date}</td>
+                                    <td>{entry.time}</td>
+                                    <td>{entry.soreThroat}</td>
+                                    <td>{entry.cough}</td>
+                                    <td>{entry.breathing}</td>
+                                    <td><button onClick={handleDelete}>DELETE</button></td>
+                                </tr>
+                            })}
+                        </thead>
+                    </table>
+                }
 
+ </div>
         </div>
     )
 }
